a19d1cde8f1f27b69b55448f3f43534151aadef1
Boby Baghel <boby9758650@gmail.com>
2023-11-25 00:54:52 +0530

    added axios with redux  cryptoTable

--- src/Componets/CryptoTable.jsx
+++ src/Componets/CryptoTable.jsx
@@ -1,14 +1,68 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+import { fetchCryptoList } from "../Repository/CryptoRepository";
+
 function CryptoTable() {
-    return (
-      <div className="flex flex-col items-center justify-center gap-5 w-[80vw] mx-auto my-5 ">
-            <div className="w-full bg-yellow-400 text-black text-xl flex items-center justify-between px-5 py-3 font-semibold rounded-lg  " >
-                <div>Coin</div>
-                <div>Price</div>
-                <div>24h Change</div>
-                <div>Market up</div>
-       </div>
-      </div>
-    );
+  const { appCurrency } = useSelector((state) => state.currency);
+
+  const [coins, setcoins] = useState([
+    {
+      id: "bitcoin",
+      symbol: "btc",
+      name: "Bitcoin",
+      image: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
+      change24h: 2.15,
+      price: 3142437,
+      market_cap: 61785170251310,
+    }
+  ]);
+
+  async function loadCryptos(appCurrency, page) {
+    const result = await fetchCryptoList(appCurrency, page, true);
+    console.log(result);
+    if (result.success) {
+       setcoins(result.data);
+    }
   }
-  export default CryptoTable;
-  
+
+  useEffect(() => {
+    loadCryptos(appCurrency);
+  }, [appCurrency]);
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-5 w-[80vw] mx-auto my-5 ">
+      <div className="w-full bg-yellow-400 text-black text-xl flex items-center justify-between px-5 py-3 font-semibold rounded-lg  ">
+        {/* headers */}
+        <div className="basis-[35%]" >Coin</div>         
+        <div className="basis-[25%]" >Price</div>
+        <div className="basis-[20%]" >24h Change</div>
+        <div className="basis-[20%]" >Market up</div>
+      </div>
+      <div className="flex flex-col w-[80vw] mx-auto " >
+        {
+          coins.map((coin) => {
+            return (
+              <div key={coin.id} className="w-full bg-transparent flex items-center justify-between text-white font-semibold px-2 py-4" >
+                <div className="flex items-center justify-start gap-3 basis-[35%]" >
+                  <div className="w-[5rem] h-[5rem] " >
+                    <img src={coin.image} className="w-full h-full " />
+                  </div>
+                  <div className="flex flex-col " >
+                    <div className="text-3xl" > {coin.name} </div>
+                    <div className="text-xl font-light" > {coin.symbol} </div>
+                  </div>
+                </div>
+                <div className="basis-[25%]"> {coin.price} </div>
+                <div className="basis-[20%]"> {coin.change24h} </div>
+                <div className="basis-[20%]"> {coin.market_cap} </div>
+
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  );
+}
+export default CryptoTable;
---
+++ src/Repository/CryptoRepository.js
@@ -0,0 +1,31 @@
+import axiosInstance from "../Config/axiosInstance";
+
+export const fetchCryptoList = async (appCurrency, page, flag = false) => {
+  try {
+    if (!flag) return;
+    const response = await axiosInstance.get(
+      `coins/markets?vs_currency=${appCurrency}&order=market_cap_desc&per_page=10&page=${page}&sparkline=false&locale=en`
+    );
+    console.log(response.data);
+    return {
+      success: true,
+      data: response.data.map((coin) => {
+        return {
+          id: coin.id,
+          symbol: coin.symbol,
+          name: coin.name,
+          image: coin.image,
+          change24h: coin.high_24h - coin.log_24h,
+          price: coin.current_price,
+          market_cap: coin.market_cap,
+        };
+      }),
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      success: false,
+      data: [],
+    };
+  }
+};
